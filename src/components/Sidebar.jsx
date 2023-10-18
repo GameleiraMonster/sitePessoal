3e33f8a889574bfd427310055cbc1ed5fcfbba39
Jânio Gameleira <jsgj1@aluno.ifal.edu.br>
2023-10-18 11:14:27 -0300

    iniciando o sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,17 @@
+import Avatar from '../img/eu.jpeg'
+import '../styles/components/sidebar.sass'
+
 const SideBar = () => {
-  return (
-    <div>SideBar</div>
+  return ( 
+  <aside id="sidebar">
+    <img src={Avatar} alt="Jânio Gameleira" />
+    <p className="title">Desenvolvedor</p>
+    <p>redes sociais</p>
+    <p>Informações de contato</p>
+    <a href="" className="btn">
+      Download Curriculo
+    </a>
+  </aside>
   )
 }
 
